28e637a05388a422fbcb6e09ff2becbcd4c833d9
ShahadNasser <Shahad.N.Alhanaya@gmail.com>
2021-12-02 17:50:26 +0300

    Done!

--- TTT/Board.swift
+++ TTT/Board.swift
@@ -2,47 +2,181 @@ import Foundation
 import UIKit
 
 class Board{
-    var blocks: [Block]
-    var turn: Turn
-    var winner: Turn
+    //a board has 9 blocks
+    private var block1 = Block(tag: "1", status: BlockStatus.none)
+    private var block2 = Block(tag: "2", status: BlockStatus.none)
+    private var block3 = Block(tag: "3", status: BlockStatus.none)
+    private var block4 = Block(tag: "4", status: BlockStatus.none)
+    private var block5 = Block(tag: "5", status: BlockStatus.none)
+    private var block6 = Block(tag: "6", status: BlockStatus.none)
+    private var block7 = Block(tag: "7", status: BlockStatus.none)
+    private var block8 = Block(tag: "8", status: BlockStatus.none)
+    private var block9 = Block(tag: "9", status: BlockStatus.none)
     
-    init(){
-        blocks = [
-            Block(tag: "1", status: BlockStatus.none),
-            Block(tag: "2", status: BlockStatus.none),
-            Block(tag: "3", status: BlockStatus.none),
-            Block(tag: "4", status: BlockStatus.none),
-            Block(tag: "5", status: BlockStatus.none),
-            Block(tag: "6", status: BlockStatus.none),
-            Block(tag: "7", status: BlockStatus.none),
-            Block(tag: "8", status: BlockStatus.none),
-            Block(tag: "9", status: BlockStatus.none),
-        ]
-        turn = Turn.red
-        winner = Turn.red
+    //current turn of game, it always starts with red
+    private var turn = Turn.red
+    
+    //returns the turn since it is private
+    func getTurn()-> Turn{
+        return turn
     }
     
-    check 
+    //change value of the turn
+    func changeTurn(){
+        if(turn == Turn.red){
+            turn = Turn.blue
+        }else {
+            turn = Turn.red
+        }
+    }
     
+    //this return the blocked status based on turn
+    func getTurnBlockStatus()-> BlockStatus{
+        if(turn == Turn.red) {
+            return BlockStatus.red
+            
+        }
+        else {
+            return BlockStatus.blue
+            
+        }
+    }
     
+    //this change specific block status based on tag
+    func changeBlockStatus(tag: Int, status: BlockStatus){
+        switch tag{
+        case 1:  block1.status = status
+        case 2:  block2.status = status
+        case 3:  block3.status = status
+        case 4:  block4.status = status
+        case 5:  block5.status = status
+        case 6:  block6.status = status
+        case 7:  block7.status = status
+        case 8:  block8.status = status
+        case 9:  block9.status = status
+        default:  print("no such block")
+        }
+    }
+    
+    //this returns specific block status based on tag
+    func getBlockStatus(tag: Int)-> BlockStatus{
+        switch tag{
+        case 1: return block1.status
+        case 2: return block2.status
+        case 3: return block3.status
+        case 4: return block4.status
+        case 5: return block5.status
+        case 6: return block6.status
+        case 7: return block7.status
+        case 8: return block8.status
+        case 9: return block9.status
+        default: return BlockStatus.none
+        }
+    }
+    
+    //checks rows, columns, and diagonals
+    func checkWin()-> Bool{
+        let color = getTurnBlockStatus()
+        if(checkRows(color: color) || checkColumns(color: color) || checkDiagonals(color: color)){
+            return true
+        }
+        return false
+    }
+    
+    //check if all blocks were pressed and not empty, in this case the game is considered over
+    func checkGameOver()-> Bool{
+        if(block1.status != BlockStatus.none &&
+           block2.status != BlockStatus.none &&
+           block3.status != BlockStatus.none &&
+           block4.status != BlockStatus.none &&
+           block5.status != BlockStatus.none &&
+           block6.status != BlockStatus.none &&
+           block7.status != BlockStatus.none &&
+           block8.status != BlockStatus.none &&
+           block9.status != BlockStatus.none ){
+            
+            return true
+        }
+        return false
+    }
+    
+    //check wining row combinations for a specific color
+    func checkRows(color: BlockStatus)-> Bool{
+        if(block1.status == color && block2.status == color && block3.status == color){
+            return true
+        }
+        else if(block4.status == color && block5.status == color && block6.status == color){
+            return true
+        }
+        else if(block7.status == color && block8.status == color && block9.status == color){
+            return true
+        }
+        return false
+    }
+    
+    //check wining column combinations for a specific color
+    func checkColumns(color: BlockStatus)-> Bool{
+        if(block1.status == color && block4.status == color && block7.status == color){
+            return true
+        }
+        else if(block2.status == color && block5.status == color && block8.status == color){
+            return true
+        }
+        else if(block3.status == color && block6.status == color && block9.status == color){
+            return true
+        }
+        return false
+    }
+    
+    //check wining diagonal combinations for a specific color
+    func checkDiagonals(color: BlockStatus)-> Bool{
+        if(block1.status == color && block5.status == color && block9.status == color){
+            return true
+        }
+        else if(block3.status == color && block5.status == color && block7.status == color){
+            return true
+        }
+        return false
+    }
+    
+    //resets all board values
+    func resetBoard(){
+        block1.status = BlockStatus.none
+        block2.status = BlockStatus.none
+        block3.status = BlockStatus.none
+        block4.status = BlockStatus.none
+        block5.status = BlockStatus.none
+        block6.status = BlockStatus.none
+        block7.status = BlockStatus.none
+        block8.status = BlockStatus.none
+        block9.status = BlockStatus.none
+        
+        turn = Turn.red
+    }
 }
 
+//each button is represented in a block
 class Block{
+    //tag that reflects button tag
     var tag: String
+    //status can indicate color or empty
     var status: BlockStatus
     
     init(tag: String, status: BlockStatus){
         self.tag = tag
         self.status = status
     }
+    
+    
 }
 
 enum BlockStatus{
-    case none
-    case red
-    case blue
+    case none //not pressed yet
+    case red //pressed by red
+    case blue //pressed by blue
 }
 
+//players turns
 enum Turn{
     case red
     case blue
--- TTT/ViewController.swift
+++ TTT/ViewController.swift
@@ -8,60 +8,120 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Game Labels
     @IBOutlet weak var turnLabel: UILabel!
     @IBOutlet weak var winnerLabel: UILabel!
+    
+    //Game Buttons
     @IBOutlet weak var resetButton: UIButton!
     
-    @IBOutlet weak var TLButton: UIButton!
-    @IBOutlet weak var TCButton: UIButton!
-    @IBOutlet weak var TRButton: UIButton!
+    @IBOutlet weak var block1: UIButton!
+    @IBOutlet weak var block2: UIButton!
+    @IBOutlet weak var block3: UIButton!
     
-    @IBOutlet weak var CLButton: UIButton!
-    @IBOutlet weak var CCButton: UIButton!
-    @IBOutlet weak var CRButton: UIButton!
+    @IBOutlet weak var block4: UIButton!
+    @IBOutlet weak var block5: UIButton!
+    @IBOutlet weak var block6: UIButton!
     
-    @IBOutlet weak var BLButton: UIButton!
-    @IBOutlet weak var BCButton: UIButton!
-    @IBOutlet weak var BRButton: UIButton!
+    @IBOutlet weak var block7: UIButton!
+    @IBOutlet weak var block8: UIButton!
+    @IBOutlet weak var block9: UIButton!
     
-//    var turn = Turn.Red
+    //Other variables
+    var board = Board() // this class holds the data of the game
+    var gameOver = false //indicator for when game is over (either with a Win or a Draw)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //start a new game when view load
         resetGame()
-        
+    }
+    
+    @IBAction func boardPressed(_ sender: UIButton) {
+        //check if blocked has not been pressed and game is not over
+        if(board.getBlockStatus(tag: sender.tag) == BlockStatus.none && !gameOver){
+            
+            //get block status (red or blue) based on turn
+            let status = board.getTurnBlockStatus()
+            
+            //change block status in board class
+            board.changeBlockStatus(tag: sender.tag, status: status)
+            //change block UI color based on status
+            if(status == BlockStatus.red) {
+                sender.backgroundColor = UIColor(named: "game_red")
+            }
+            else {
+                sender.backgroundColor = UIColor(named: "game_blue")
+            }
+            
+            //check if a player won or not
+            if(board.checkWin()){
+                //in case a player wins, end the game (this means players wont be able to click on empty blocks)
+                gameOver = true
+                
+                //change UI of game based on winner
+                if(board.getTurn() == Turn.red){
+                    winnerLabel.text = "Red Wins!"
+                    winnerLabel.textColor = UIColor(named: "game_red")
+                    winnerLabel.isHidden = false
+                    turnLabel.isHidden = true
+                }else{
+                    winnerLabel.text = "Blue Wins!"
+                    winnerLabel.textColor = UIColor(named: "game_blue")
+                    winnerLabel.isHidden = false
+                    turnLabel.isHidden = true
+                }
+                
+            }else{
+                //else no one wins
+                //check if the game is over (this means all blocks has been pressed and no empty blocks)
+                if(board.checkGameOver()){
+                    //change UI of game to Draw
+                    winnerLabel.text = "Draw!"
+                    winnerLabel.textColor = UIColor.systemIndigo
+                    winnerLabel.isHidden = false
+                    turnLabel.isHidden = true
+                }else{
+                    //else the game is not over change to other player turn
+                    board.changeTurn()
+                    //change turn label based on current turn
+                    if(board.getTurn() == Turn.red){
+                        turnLabel.text = "Red turn"
+                        turnLabel.textColor = UIColor(named: "game_red")
+                    }else{
+                        turnLabel.text = "Blue turn"
+                        turnLabel.textColor = UIColor(named: "game_blue")
+                    }
+                }
+            }
+            
+        }
     }
     
     @IBAction func resetButtonPressed(_ sender: UIButton) {
         resetGame()
-        
-    }
-    
-    
-    func startGame(){
-        
     }
     
     func resetGame(){
-//        turn = Turn.Red
         turnLabel.text = "Red Starts!"
-        winnerLabel.text = "Winner"
+        turnLabel.textColor = UIColor(named: "game_red")
+        turnLabel.isHidden = false
         winnerLabel.isHidden = true
+        gameOver = false
+        board.resetBoard()
         
-        TLButton.backgroundColor = UIColor(named: "beige_buttons")
-        TCButton.backgroundColor = UIColor(named: "beige_buttons")
-        TRButton.backgroundColor = UIColor(named: "beige_buttons")
+        block1.backgroundColor = UIColor(named: "beige_buttons")
+        block2.backgroundColor = UIColor(named: "beige_buttons")
+        block3.backgroundColor = UIColor(named: "beige_buttons")
         
-        CLButton.backgroundColor = UIColor(named: "beige_buttons")
-        CCButton.backgroundColor = UIColor(named: "beige_buttons")
-        CRButton.backgroundColor = UIColor(named: "beige_buttons")
+        block4.backgroundColor = UIColor(named: "beige_buttons")
+        block5.backgroundColor = UIColor(named: "beige_buttons")
+        block6.backgroundColor = UIColor(named: "beige_buttons")
         
-        BLButton.backgroundColor = UIColor(named: "beige_buttons")
-        BCButton.backgroundColor = UIColor(named: "beige_buttons")
-        BRButton.backgroundColor = UIColor(named: "beige_buttons")
+        block7.backgroundColor = UIColor(named: "beige_buttons")
+        block8.backgroundColor = UIColor(named: "beige_buttons")
+        block9.backgroundColor = UIColor(named: "beige_buttons")
     }
-    
-    
 }
 
 
